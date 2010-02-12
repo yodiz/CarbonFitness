958e234f23085c1126fcccab373fc2b4aaa60968
unknown <Robert Macfie@.AVEGA.DNS>
2010-02-12 11:35:27 +0100

    Added Design Mockups

---
+++ CarbonFitnessTest.Web/Controller/HomeController/IndexTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CarbonFitnessTest.Web.Controller.HomeController
+{
+	[TestFixture]
+	public class IndexTest
+	{
+		[Test]
+		public void shouldShowFoodLinkAfterLogin()
+		{
+
+			//Assert(foodLink, )
+		}
+	}
+}
