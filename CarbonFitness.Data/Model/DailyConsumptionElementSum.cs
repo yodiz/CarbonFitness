51639aa3838dde3f6b06a1ba11040f9cd66f7be2
unknown <Tomas@.(none)>
2010-03-15 01:09:46 +0100

    New tests for result page

---
+++ CarbonFitness.Data/Model/DailyConsumptionElementSum.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CarbonFitness.Data.Model {
+    public class DailyConsumptionElementSum {
+        public int Value{ get; set;}
+        public DateTime Date { get; set; }
+    }
+}
