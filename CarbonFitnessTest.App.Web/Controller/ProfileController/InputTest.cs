bdcaaa3c02bfe949a60036979cff46465c44c1d8
unknown <MikaelKjellqvist@.AVEGA.DNS>
2010-03-15 14:37:24 +0100

    Temp checkin

---
+++ CarbonFitnessTest.App.Web/Controller/ProfileController/InputTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CarbonFitness.BusinessLogic;
+using CarbonFitness.Data.Model;
+using CarbonFitness.DataLayer.Repository;
+using Moq;
+using NUnit.Framework;
+
+namespace CarbonFitnessTest.Web.Controller.ProfileController
+{
+    [TestFixture]
+    public class InputTest
+    {
+        [Test]
+        public void shouldSaveIdealWeightValue() {
+
+
+            var userRepositoryMock = new Mock<IUserBusinessLogic>(MockBehavior.Strict);
+            userRepositoryMock.Setup(x => x.SaveOrUpdate(It.IsAny<User>())).Returns(new User());
+
+            var profileController = new CarbonFitness.App.Web.Controllers.ProfileController(); 
+            profileController.Input()
+
+        }
+    }
+}
